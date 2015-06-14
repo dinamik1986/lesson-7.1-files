ef1efed7f4a0dba0fd5877527e2753a2c493f970
Dmitriy Potekhin <potekhin1986@gmail.com>
2015-06-14 17:47:16 +0700

    1234

--- index.php
+++ index.php
@@ -5,11 +5,55 @@ ini_set('display_errors', 1);
 
 session_start();
 
+
+
+if(isset($_POST['main_form_submit']) && $_POST['main_form_submit']=="Отправить"){
+        foreach ($_POST as $id=>$val){
+            if(empty($val) && $val != 'allow_mails'){
+                show_form();
+                exit ('Введите значение '.$id.' в форму!!!');           
+            }        
+        }      
+    check();   
+    cookies_add();    
+}
+
+
+if(isset($_POST['main_form_submit']) && $_POST['main_form_submit']=="Сохранить"){
+    foreach ($_POST as $par=>$val){
+        if(empty($val) && $val != 'allow_mails'){
+                show_form();
+                exit ('Введите значение '.$par.' в форму!!!');
+        }
+    }
+    
+check();
+$mass_ads=preobr();
+$mass_ads[$_GET['id']]=$_POST;
+obr_preobr($mass_ads);
+}
+
+
+function cookies_add(){
+
+        if (isset ($_COOKIE['ads'])){
+                $post=$_COOKIE['ads'];
+                $post=$post.'|'.serialize($_POST);
+        }
+        else{
+            $post=$_POST;
+            $post=serialize($post);
+        }
+        setcookie('ads',$post,time()+3600*24*7);
+}
+
+
 if(isset($_GET['action'])){
     switch($_GET['action']){
         case 'delete':{
             if(isset($_GET['id'])){
-                unset($_SESSION['ads'][(int)$_GET['id']]);
+                $id = (int)$_GET['id'];
+                del_ads($id);
                 header('Location: '.$_SERVER['PHP_SELF']);
                 exit;
             }
@@ -18,44 +62,71 @@ if(isset($_GET['action'])){
         case 'edit':{
             if(isset($_GET['id'])){
                 $id = (int)$_GET['id'];
-                if(isset($_SESSION['ads'][$id])){
-                    show_form($id);
-                }
+                edit_ads($id);
             }
             break;
         }
     }
-}else{
+}
+else{
     show_form();
 }
 
 
-
-
-
-function getFullList()
-{
-    
-    if(count($_SESSION['ads']))
-    {
-        $ads = $_SESSION['ads'];
-        foreach($ads as $key=>$val)
-        {
-            echo "<a href='?action=edit&id=$key'>".$val['title']."</a> - <a href='?action=delete&id=$key'>удалить</a><br>";
+function edit_ads($id){
+    $mass_ads=preobr();
+        if(isset($mass_ads[$id])){
+            show_form($id,$mass_ads[$id]);
         }
-        
-    }
-    
-    else
-    {
+}
+
+
+function del_ads($id){
+    $mass_ads=preobr();
+    array_splice($mass_ads,$id,1);
+    obr_preobr($mass_ads);
+}
+
+
+function getFullList(){
+    if (isset ($_COOKIE['ads'])){
+        $mass_ads=preobr();
+    } 
+    if(count($mass_ads)){
+        foreach($mass_ads as $key=>$val){
+            echo "<a href='?action=edit&id=$key'>".$val['title']."</a> - <a href='?action=delete&id=$key'>удалить</a><br>";
+        }   
+    }    
+    else{
         echo "Объявлений нет";
     }
     
 }
 
 
+function preobr(){   
+ $ads = explode("|", $_COOKIE['ads']); 
+            foreach ($ads as $key=>$val){              
+                $val=unserialize($val);
+                $mass_ads[]=$val;
+            }            
+            return $mass_ads;    
+}
 
-function show_form($id=false){
+
+
+function obr_preobr($mass_ads){    
+        foreach ($mass_ads as $key=>$val){              
+              $val=serialize($val);
+              $ads_new[]=$val;
+        }
+    $ads_cookie=implode("|", $ads_new);    
+    setcookie('ads',$ads_cookie,time()+3600*24*7);    
+}
+
+
+
+function show_form($id=false,$mass_ads=false){
     // вытащить объявление из базы (массив сессии)
     //распихать значения объявления по форме
     $ad['seller_name']='';
@@ -73,8 +144,7 @@ function show_form($id=false){
     $ad['allow_mails'] = '';
     
     
-    if (!empty($_POST))
-    {
+    if (!empty($_POST)){
         $ad['seller_name']=$_POST['seller_name'];
         $ad['email']=$_POST['email'];
         $ad['price']=$_POST['price'];
@@ -86,100 +156,88 @@ function show_form($id=false){
         $metka = $_POST['category_id'];
         
         
-        if ($_POST['private']=='1')
-        {
+        if ($_POST['private']=='1'){
             $ad['private1'] = 'checked';
             $ad['private2'] = '';
         }
-        else
-        {
+        else{
          $ad['private1'] = '';
          $ad['private2'] = 'checked';
-        }
-        
+        }        
     }
     
     
     
 
-    if(isset($id) && is_numeric($id)) 
-    {
-        $ad = $_SESSION['ads'][$id];
+    if(isset($id) && is_numeric($id)){
+        $ad = $mass_ads;
         $ad['main_form_submit']='Сохранить';
-        $gorod = $_SESSION['ads'][$id]['location_id'];
-        $stanciya = $_SESSION['ads'][$id]['metro_id'];
-        $metka = $_SESSION['ads'][$id]['category_id'];
+        $gorod = $ad['location_id'];
+        $stanciya = $ad['metro_id'];
+        $metka = $ad['category_id'];
         
-        if ($_SESSION['ads'][$id]['private']=='1')
-        {
-            $ad['private1'] = 'checked';
-            $ad['private2'] = '';
-        }
-        else
-        {
-         $ad['private1'] = '';
-         $ad['private2'] = 'checked';
-        }
+            if ($ad['private']=='1'){
+                $ad['private1'] = 'checked';
+                $ad['private2'] = '';
+            }
+            else{
+                $ad['private1'] = '';
+                $ad['private2'] = 'checked';
+            }    
+            if (isset($ad['allow_mails'])){
+                $ad['allow_mails'] = 'checked';
+            }        
+             else{
+                 $ad['allow_mails'] = '';
+            }   
+    }
     
-    if (isset($_SESSION['ads'][$id]['allow_mails']))
-        {
-            $ad['allow_mails'] = 'checked';
-        }
-        
-        else
-        {
-              $ad['allow_mails'] = '';
-        }
+    $citys = array(
+        '641780'=>'Новосибирск',
+        '641490'=>'Барабинск',
+        '641510'=>'Бердск',
+        '641600'=>'Искитим',
+        '641630'=>'Колывань',
+        '641680'=>'Краснообск',
+        '641710'=>'Куйбышев',
+        '641760'=>'Мошково',
+        '641790'=>'Обь',
+        '641800'=>'Ордынское',
+        '641970'=>'Черепаново');
     
-    
-       }
-    
-        $citys = array(
-    '641780'=>'Новосибирск',
-    '641490'=>'Барабинск',
-    '641510'=>'Бердск',
-    '641600'=>'Искитим',
-    '641630'=>'Колывань',
-    '641680'=>'Краснообск',
-    '641710'=>'Куйбышев',
-    '641760'=>'Мошково',
-    '641790'=>'Обь',
-    '641800'=>'Ордынское',
-    '641970'=>'Черепаново');
-    
-        $stations = array(
-    '2028'=>'Берёзовая роща',
-    '2018'=>'Гагаринская',
-    '2017'=>'Заельцовская',
-    '2029'=>'Золотая Нива',
-    '2019'=>'Красный проспект',
-    '2027'=>'Маршала Покрышкина',
-    '2021'=>'Октябрьская',
-    '2025'=>'Площадь Гарина-Михайловского',
-    '2020'=>'Площадь Ленина',
-    '2024'=>'Площадь Маркса',
-    '2022'=>'Речной вокзал',
-    '2026'=>'Сибирская',
-    '2023'=>'Студенческая');         
+    $stations = array(
+        '2028'=>'Берёзовая роща',
+        '2018'=>'Гагаринская',
+        '2017'=>'Заельцовская',
+        '2029'=>'Золотая Нива',
+        '2019'=>'Красный проспект',
+        '2027'=>'Маршала Покрышкина',
+        '2021'=>'Октябрьская',
+        '2025'=>'Площадь Гарина-Михайловского',
+        '2020'=>'Площадь Ленина',
+        '2024'=>'Площадь Маркса',
+        '2022'=>'Речной вокзал',
+        '2026'=>'Сибирская',
+        '2023'=>'Студенческая');         
 
           
-         $labels_1 = array(
-    '9'=>'Автомобили с пробегом',
-    '109'=>'Новые автомобили',
-    '14'=>'Мотоциклы и мототехника',
-    '81'=>'Грузовики и спецтехника',
-    '11'=>'Водный транспорт',
-    '10'=>'Запчасти и аксессуары');
+    $labels_1 = array(
+        '9'=>'Автомобили с пробегом',
+        '109'=>'Новые автомобили',
+        '14'=>'Мотоциклы и мототехника',
+        '81'=>'Грузовики и спецтехника',
+        '11'=>'Водный транспорт',
+        '10'=>'Запчасти и аксессуары');
              
              
-        $labels_2 = array(
-    '24'=>'Квартиры',
-    '23'=>'Комнаты',
-    '25'=>'Дома, дачи, коттеджи',
-    '26'=>'Земельные участки',
-    '85'=>'Гаражи и машиноместа',
-    '42'=>'Коммерческая недвижимость',
-    '86'=>'Недвижимость за рубежом');   
+    $labels_2 = array(
+        '24'=>'Квартиры',
+        '23'=>'Комнаты',
+        '25'=>'Дома, дачи, коттеджи',
+        '26'=>'Земельные участки',
+        '85'=>'Гаражи и машиноместа',
+        '42'=>'Коммерческая недвижимость',
+        '86'=>'Недвижимость за рубежом');   
              
               
 ?>
@@ -226,8 +284,7 @@ function show_form($id=false){
             <option class="opt-group" disabled="disabled">-- Города --</option>
 <?php
                 
-                    foreach($citys as $number=>$city)
-                    {
+                    foreach($citys as $number=>$city){
                         $selected_c = ($number==$gorod) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
                         echo '<option data-coords=",," '.$selected_c.' value="'.$number.'">'.$city.'</option>';
                     }
@@ -242,9 +299,8 @@ function show_form($id=false){
                 <option value="">-- Выберите станцию метро --</option>
 <?php
                 
-                    foreach($stations as $number=>$station)
-                    {
-                        $selected = ($number==$stanciya) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
+                    foreach($stations as $number=>$station){
+                        $selected = ($number==$stanciya) ? 'selected=""' : ''; 
                         echo '<option data-coords=",," '.$selected.' value="'.$number.'">'.$station.'</option>';
                     }
                   
@@ -263,9 +319,8 @@ function show_form($id=false){
             <optgroup label="Транспорт">
 <?php
                 
-                    foreach($labels_1 as $number=>$lable)
-                    {
-                        $selected = ($number==$metka) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
+                    foreach($labels_1 as $number=>$lable){
+                        $selected = ($number==$metka) ? 'selected=""' : ''; 
                         echo '<option data-coords=",," '.$selected.' value="'.$number.'">'.$lable.'</option>';
                     }
                   
@@ -276,9 +331,8 @@ function show_form($id=false){
 
 <?php
                 
-                    foreach($labels_2 as $number=>$lable)
-                    {
-                        $selected = ($number==$metka) ? 'selected=""' : ''; //если мы передали в функцию город который нужно выставить в списке то мы ставим специальную метку в селектор
+                    foreach($labels_2 as $number=>$lable){
+                        $selected = ($number==$metka) ? 'selected=""' : ''; 
                         echo '<option data-coords=",," '.$selected.' value="'.$number.'">'.$lable.'</option>';
                     }
                   
@@ -330,78 +384,31 @@ function show_form($id=false){
 <?php
 }
 
-if(isset($_POST['main_form_submit']) && $_POST['main_form_submit']=="Отправить")
 
-{
-        foreach ($_POST as $id=>$val)
-    {
-        if(empty($val) && $val != 'allow_mails') 
-        {
-                exit ('Введите значение '.$id.' в форму!!!');
-                
-        }
-        
-        
-    }
-    
-    if (preg_match('/\D+/', $_POST['price']) == 1) 
-    {
+
+
+
+
+if(isset($_COOKIE['ads'])){
+    getFullList();
+}
+
+
+
+
+
+function check(){    
+     if (preg_match('/\D+/', $_POST['price']) == 1){
         exit ('Цена должна быть числом!');
     }
     
-     if (! preg_match('/^\+?[0-9]{7,10}$/', $_POST['phone']) == 1) 
-    {
+     if (! preg_match('/^\+?[0-9]{7,10}$/', $_POST['phone']) == 1){
         exit ('Введите корректный 10-значный номер мобильного телефона!');
     }
     
-     if (! preg_match( '/^[-0-9a-z_\.]+@[-0-9a-z^\.]+\.[a-z]{2,4}$/i', $_POST['email'])) 
-    {
+     if (! preg_match( '/^[-0-9a-z_\.]+@[-0-9a-z^\.]+\.[a-z]{2,4}$/i', $_POST['email'])){
         exit ('Введите корректный email!!');
-    }
-    
-$_SESSION['ads'][]=$_POST;
-
+    }  
 }
-
-
-if(isset($_POST['main_form_submit']) && $_POST['main_form_submit']=="Сохранить")
-
-{
-    foreach ($_POST as $par=>$val)
-    {
-        if(empty($val) && $val != 'allow_mails') 
-        {
-                exit ('Введите значение '.$par.' в форму!!!');
-                
-        }
-    }
-    
-     if (preg_match('/\D+/', $_POST['price']) == 1) 
-    {
-        exit ('Цена должна быть числом!');
-    }
-    
-     if (! preg_match('/^\+?[0-9]{7,10}$/', $_POST['phone']) == 1) 
-    {
-        exit ('Введите корректный 10-значный номер мобильного телефона');
-    }
-    
-    if (! preg_match( '/^[-0-9a-z_\.]+@[-0-9a-z^\.]+\.[a-z]{2,4}$/i', $_POST['email'])) 
-    {
-        exit ('Введите корректный email!!');
-    }
-    
-$id_red = $_GET['id'];
-$_SESSION['ads'][$id_red]=$_POST;
-}
-
-
-if (!empty($_SESSION))
-getFullList();
-
-
 
 ?>
-
-
-
